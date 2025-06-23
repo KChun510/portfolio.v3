a5fbff8dd45f4986f324e7b81e3dae8622e09b1b
Kchun510 <kylechun1@gmail.com>
2025-06-23 14:35:59 -0700

    Next js bloat

--- src/app/api/spotify/remove_track_from_playlist/route.tsx
+++ src/app/api/spotify/remove_track_from_playlist/route.tsx
@@ -4,7 +4,7 @@ import { remove_track_from_playlist } from '../../../spotify_utils/api_endpoints
 export async function POST(req: NextRequest) {
 	const body = await req.json()
 	try {
-		const success = await remove_track_from_playlist()
+		const success = await remove_track_from_playlist([{ uri: "test" }])
 		if (!success) {
 			throw new Error("Failed to remove track from playlist")
 		}
