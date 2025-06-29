ff4f68aeedbbaf29b9287389c35b56faf7b3ff9f
Kchun510 <kylechun1@gmail.com>
2025-06-29 11:25:04 -0700

    CronScript update, need to make the injected .env vars avail to the cron process

--- docker_scripts/entrypoint.sh
+++ docker_scripts/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+# Export current env into a file cron reads
+printenv | grep -v "no_proxy" > /etc/environment
+
+# Optional: load it for interactive shell processes too
+set -o allexport
+. /etc/environment
+set +o allexport
+
 # Start cron
 cron
 
-# Optional: log cron status
 echo "Cron started..."
 
-# Start the Next.js app
+# Start Next.js app
 npx next start -H 0.0.0.0
-
 
