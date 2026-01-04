a894261fe11c6e902b5516168ee27d4672600f3f
Kchun510 <kylechun1@gmail.com>
2026-01-04 07:31:34 -0800

    Defer secret access, until run time
    Builds failing, trying to access .env var that does not yet exist

--- src/app/openai_utils/openAI_api.ts
+++ src/app/openai_utils/openAI_api.ts
@@ -2,9 +2,10 @@ import { OpenAI } from "openai"
 import path from 'path';
 import dotenv from 'dotenv'
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
 export async function analyze_userTag(user_tag: string) {
+	const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+
 	const meta_data = await openai.chat.completions.create({
 		messages: [{ role: "system", content: "You are tasked with analyzing a user tag for my website, that is user-created. Determine if it's professional/ acceptable if a recruiter were to see the user tag. Nicknames and playful tags are allowed. Your output will be 'true': if acceptable or 'false': if denied" },
 		{ role: "user", content: `Here is the user tag analyze: ${user_tag}` }],
