aa42b0d43bc05b0c894a60d1cc64eb3dd41ee065
Kchun510 <kylechun1@gmail.com>
2026-01-11 18:37:29 -0800

    Bug Fix: Invalid error thrown, when no song playing

--- src/app/api/spotify/get_current_track/route.tsx
+++ src/app/api/spotify/get_current_track/route.tsx
@@ -5,7 +5,7 @@ export async function GET() {
 	try {
 		const data = await get_current_track()
 		if (!data) {
-			throw new Error("Failed to get current track")
+			console.log("Failed to get current track")
 		}
 		return NextResponse.json(data, { status: 200 })
 	} catch (err) {
